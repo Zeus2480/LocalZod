fc21ccf30a9caae95c9e2e7072d97c76063a8065
Zeus2480 <89025631+Zeus2480@users.noreply.github.com>
2021-09-07 16:05:07 +0530

    Add files via upload

---
+++ Localzod(project)/js/main.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+function initMap() {
+    // Your location
+    const loc = { lat: 26.88800732115955, lng: 80.96324551811972 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 14,
+      center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+  }
+  
+  $('#container a, .btnn a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
